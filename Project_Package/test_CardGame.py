ebf16ee2be4b5ab1dc6253bbdc420af02334e0f7
sagi bondarenko <sagibon@gmail.com>
2020-12-07 09:07:46 +0200

    updated my test classes

--- Project_Package/test_CardGame.py
+++ Project_Package/test_CardGame.py
@@ -11,11 +11,13 @@ class TestCardGame(TestCase):
 
     def test_new_game(self):
         self.test_game.new_game()
-        self.test_game.new_game()
         self.assertEqual(self.test_game.new_game ,1)
 
     def test_get_winner(self):
         self.test_game.player1.cardList = []  # checking if player 1 does win if his list is shorter
         self.test_game.player2.cardList = [1, 2, 3, 4, 5, 6]
         self.assertEqual(self.test_game.get_winner(), self.test_game.player1)
-        print("The method returns the expected value")
+        self.test_game.player1.cardList = []  # checking if players have equal card amount if return None
+        self.test_game.player2.cardList = []
+        self.assertEqual(self.test_game.get_winner(), None)
+        print("The method get_winner method returns the expected value")
---
+++ Project_Package/test_Player.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestPlayer(TestCase):
+    def test_set_hand(self):
+        self.fail()
+
+    def test_get_card(self):
+        self.fail()
+
+    def test_add_card(self):
+        self.fail()
+
+    def test_show(self):
+        self.fail()
